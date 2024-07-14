aea67f5ab915d6fa14025e895a685b94fdd23331
Vilochana Rajapaksha <you@example.com>
2024-07-14 21:05:10 +0530

    client page dashboard bug fixied

--- client/src/pages/Client/Components/Pages/ClientPage.jsx
+++ client/src/pages/Client/Components/Pages/ClientPage.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import Container from '../Nav/Container'
-import Banner from '../Banner/Banner'
+import React from 'react';
+import Container from '../Nav/Container';
+import Banner from '../Banner/Banner';
+import { products } from './ClientProducts';
+import { TruncateText } from '../Utils/truncate';
 
 export default function ClientPage() {
   return (
     <div className="p-8">
-       <Container>
+      <Container>
         <div>
-            <Banner />
+          <Banner />
         </div>
-       </Container>
+        {/*retview products*/ }
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap:8">
+          {products.map((product) => {
+            return (
+              <div key={product.id}>
+                {/*reduced text length and retview*/ }
+                {TruncateText(product.name)} 
+              </div>
+            );
+          })}
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
