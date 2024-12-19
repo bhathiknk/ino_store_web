2f33410e05ad99dc6ec80d121ac712b971875d69
Maheesha Dewmini <maheeshadewmini@Maheeshas-MacBook-Air.local>
2024-12-19 23:14:52 +0530

    import libraries

--- client/src/pages/Admin/CreateProduct/ProductDetails.jsx
+++ client/src/pages/Admin/CreateProduct/ProductDetails.jsx
@@ -1,1 +1,6 @@
-//add
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
