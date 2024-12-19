61843a1a42b90cd42d583971038babe3dcd09d8d
Maneesha Dulmini <maneeshadulmini@Maneeshas-MacBook-Air.local>
2024-12-19 23:07:42 +0530

    import libraries

--- client/src/pages/Admin/CreateProduct/CreateProduct.jsx
+++ client/src/pages/Admin/CreateProduct/CreateProduct.jsx
@@ -1,1 +1,6 @@
-// add
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaArrowLeft, FaBars, FaTimes, FaTrashAlt } from 'react-icons/fa';
+import axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
