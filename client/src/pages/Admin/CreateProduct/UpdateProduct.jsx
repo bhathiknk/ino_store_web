426b1683cd7bd59c6e0c0d0b04f5a1a9a235fc82
Sayuni <118969889+Satoru24@users.noreply.github.com>
2024-12-19 23:01:56 +0530

    import libraries

--- client/src/pages/Admin/CreateProduct/UpdateProduct.jsx
+++ client/src/pages/Admin/CreateProduct/UpdateProduct.jsx
@@ -1,1 +1,6 @@
-//add
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
+import { FaArrowLeft, FaTrashAlt } from 'react-icons/fa';
