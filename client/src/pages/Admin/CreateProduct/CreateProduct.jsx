65cecd6b928991292438d56afd84966876e9d828
Maneesha Dulmini <maneeshadulmini@Maneeshas-MacBook-Air.local>
2024-12-20 21:25:50 +0530

    Create Product Details

--- client/src/pages/Admin/CreateProduct/CreateProduct.jsx
+++ client/src/pages/Admin/CreateProduct/CreateProduct.jsx
@@ -4,3 +4,30 @@ import { FaArrowLeft, FaBars, FaTimes, FaTrashAlt } from 'react-icons/fa';
 import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from 'react-toastify';
+
+const CreateProduct = () => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const [images, setImages] = useState([]);
+  const [isDiscount, setIsDiscount] = useState(false);
+  const [activeSection, setActiveSection] = useState('basic');
+  const [isFreeShipping, setIsFreeShipping] = useState(false);
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [confirmationPopupVisible, setConfirmationPopupVisible] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [individualDescriptions, setIndividualDescriptions] = useState([]);
+  const [individualPopupVisible, setIndividualPopupVisible] = useState(false);
+
+  const [formData, setFormData] = useState({
+    title: '',
+    categoryDescription: '',
+    description: '',
+    basePrice: '',
+    discountPrice: '',
+    isDiscount: false,
+    isFreeShipping: false,
+    shippingCost: '',
+    quantity: '',
+    images: []
+  });
+};
